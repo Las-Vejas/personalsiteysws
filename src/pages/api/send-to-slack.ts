8bb0cc3f2322d1aed75f60fb3c79d737ec5b86c6
vejas <118309632+Las-Vejas@users.noreply.github.com>
2025-10-29 11:09:06 +0000

    added images on the main image (yay)

--- src/pages/api/send-to-slack.ts
+++ src/pages/api/send-to-slack.ts
@@ -12,7 +12,7 @@ export async function POST({ request }: APIContext) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      text: `📩 New form submission:\nName: ${name}\nEmail: ${email}\nMessage: ${message}`
+      text: `New form submission:\nName: ${name}\nEmail: ${email}\nMessage: ${message}`
     }),
   });
 
