13a877b33f863d0f47369f3ed363a7d1aea4d032
vejas <118309632+Las-Vejas@users.noreply.github.com>
2025-11-03 18:18:44 +0000

    idk if hover works

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    colors: {
+      'text': 'var(--text)',
+      'background': 'var(--background)',
+      'primary': 'var(--primary)',
+      'secondary': 'var(--secondary)',
+      'accent': 'var(--accent)',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+}
